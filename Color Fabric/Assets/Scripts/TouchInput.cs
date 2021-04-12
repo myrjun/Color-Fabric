16dd74ab17fec10343374ec10f184a6b7ce959e6
myrjun <myriam.jung.an123@gmail.com>
2021-04-12 12:11:39 +0200

    Matinée 12/04 - Boutons fonctionnels

--- Color Fabric/Assets/Scripts/TouchInput.cs
+++ Color Fabric/Assets/Scripts/TouchInput.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchInput : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int CurrentButtonNumber;
+    
+    public Canvas Interface01;
+    public Canvas Interface02;
+    public Canvas Interface03;
+    public Canvas Interface04;
+    public Canvas Final;
+
+    public Text Result;
+
+    public int resultValue;
+
+    private Button CurrentButton;
+
+    //public ArrayList;
+
+    
     void Start()
+    {
+       
+    }
+
+    public void DisplayResult()
+    {
+        string name = EventSystem.current.currentSelectedGameObject.name;
+        int CurrentButtonNumber;
+        int.TryParse(name, out CurrentButtonNumber);
+        Debug.Log(CurrentButtonNumber);
+
+        ResultatCalcul();
+
+    }
+
+    public void ResultatCalcul()
+    {
+
+    }
+
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnChangeCanvas()
     {
-        
+
     }
 }
---
+++ Color Fabric/Assets/Scripts/calculette.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class calculette : MonoBehaviour
+{
+    public Text CurrentButtonNumber;
+    public Text Result;
+    public int resultValue;
+    private GameObject CurrentButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void DisplayResult()
+    {
+
+        Debug.Log(CurrentButtonNumber.text);
+        //Result.text = CurrentButtonNumber.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

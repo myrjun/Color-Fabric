4f024d3942c52a1e90fd7d3bdb645b569de7ac67
Fertyna <justineverstraeten@hotmail.com>
2021-04-14 13:45:13 +0200

    Interface Menu 1

---
+++ Color Fabric/Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mainmenu : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject Canvas;
+    bool Menuactive = true;
+   
+    void Update()
+    {
+
+    }
+ 
+    public void CacaProut() 
+    {
+        Menuactive = false;
+        Menubool();
+    }
+
+    public void ReturnButton()
+    {
+        Menuactive = true;
+        Menubool();
+    }
+
+
+    public void Menubool()
+    {
+        if (Menuactive == true)
+        {          
+            Panel.SetActive(true);
+            Canvas.SetActive(false);
+
+        } else if (Menuactive == false)
+        {
+            Panel.SetActive(false);
+            Canvas.SetActive(true);
+        }
+
+    }
+
+   
+}
